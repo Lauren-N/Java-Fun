4664a5c1f1141f00a901e4f31f130ae22bb2bd1c
Lauren-N <rml0729@gmail.com>
2023-01-31 13:27:49 -0800

    rudimentary string reverse

---
+++ reverseString.java
@@ -0,0 +1,18 @@
+// Lauren Nguyen
+// Personal Practice: Reversing a String
+
+public class reverseString
+{
+
+    public static void main(String args[])
+    {
+        String str = "hi";
+        System.out.print(str);
+        char letter = str.charAt(0);
+        char letterSecond = str.charAt(1);
+        System.out.println();
+        System.out.print(letterSecond);
+        System.out.print(letter);
+
+    }
+}
